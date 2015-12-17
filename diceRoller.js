c1070f515bb09ffe6d750543555b035a5bc3f30e
lackinginflavor <lackinginflavor@users.noreply.github.com>
2015-12-17 12:47:17 -0500

    fixed I/O, so all is entered within HTML

--- diceRoller.js
+++ diceRoller.js
@@ -1,10 +1,20 @@
-function diceRoll() {
-    var number = prompt("How many dice would you like to roll?");
-    var sides = prompt("How many sides on those dice?");
-    var rollArray = [];
-    for (var i = 0; i < number; i++) {
-      rollArray[i] = Math.floor(Math.random()*sides) + 1;
-      console.log(rollArray[i]);
-      }
-    alert(rollArray);
-}
+$(document).ready(function() {
+  $('#rollDice').click (function() {
+      $('#result').empty();
+      var sides = $("input[name='diceSides']").val();
+      console.log('sides: ' + sides);
+      var number = $("input[name='diceNumber']").val();
+      console.log('number: ' + number);
+      var rollArray = [];
+      var diceTotal = 0;
+      console.log('diceTotal: ' + diceTotal);
+      for (var i = 0; i < number; i++) {
+        rollArray[i] = Math.floor(Math.random()*sides) + 1;
+        console.log('die ' + i + ' ' + rollArray[i]);
+        $('#result').append('Die ' + (i+1) + ': ' + rollArray[i] +'<br>');
+        diceTotal += rollArray[i];
+        console.log(diceTotal);
+        }
+      $('#result').append('Total: ' + diceTotal);
+  });
+});
